c285ed9752d8715d8385757a16fd949a5a8af5f4
Thirumurugan <muruganraj1705@gmail.com>
2023-06-26 12:32:26 +0530

    login page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import { useSession, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <div>
-      Hi
-    </div>
-  )
+
+  const { data: session } = useSession();
+  const router = useRouter();
+  const fetch = async () => {
+    try {
+      const response = await axios.get('https://aliexpress-datahub.p.rapidapi.com/item_detail?itemId=3256804591426248', {
+        headers: {
+          'X-RapidAPI-Key': '6b6510f000mshafe4f83d7f89c8cp1bc62cjsnc5768ccf27eb',
+          'X-RapidAPI-Host': 'aliexpress-datahub.p.rapidapi.com'
+        }
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetch();
+  }, [])
+
+  if (session) {
+    return (
+      <div>{session.user.name}</div>
+    )
+  }
+  else {
+    return (
+      <div className='bg-blue-500 w-screen h-screen flex items-center justify-center'>
+        <h2 className='bg-white p-2 rounded-lg cursor-pointer' onClick={() => { router.push("/login") }}>Login With Google</h2>
+      </div>
+    )
+  }
 }
--- pages/login.js
+++ pages/login.js
@@ -1,52 +1,63 @@
-import { FontawesomeObject } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter, faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faLock, faUnlockKeyhole, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 
 const login = () => {
+
+    const [reg, setReg] = useState(false);
+
     return (
+
         <div className="md:p-8 px-2 border h-screen w-screen bg-blue-300 flex justify-center items-center">
-            <div className="flex border md:p-16 p-4 gap-3 bg-white rounded-md">
+            <div className="flex border md:p-16 p-4 gap-3 bg-white rounded-md relative h-auto pb-16">
+                <div className="absolute md:right-0 md:top-0 md:p-3 bottom-0 py-3">
+                    <p className="">{!reg ? "New User? " : "Existing User? "}<span className="text-blue-400 cursor-pointer" onClick={() => { setReg(!reg) }}>{!reg ? "Sign Up" : "Sign In"}</span></p>
+                </div>
                 <div className="w-1/2 h-full hidden md:block">
                     <img src="https://brandesignhub.com/images/quote_banner.png" className="w-full" />
                 </div>
                 <div className="md:w-1/2">
                     <p className="text-2xl md:text-4xl font-bold md:tracking-wide mb-1">Welcome Back!</p>
                     <p className="text-md md:text-lg text-gray-500 ">Login to continue</p>
-                    <div className="flex flex-col gap-5 mt-5 md:mt-10">
-                        <div className="md:mr-20 h-12 border flex px-4 gap-4">
+                    <div className={!reg ? "flex flex-col gap-5 mt-5 md:mt-10" : "flex flex-col gap-1 mt-5 md:mt-10"}>
+                        {reg && <div className="md:mr-20 h-12 border flex pl-4 gap-4">
                             <div className="h-12 flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                                </svg>
+                                <FontAwesomeIcon icon={faUser} size="lg" />
                             </div>
-                            <input className="text-lg " placeholder="Username" />
+                            <input className="text-lg w-full" type="email" placeholder="Username" />
+                        </div>}
+                        <div className="md:mr-20 h-12 border flex pl-4 gap-4">
+                            <div className="h-12 flex items-center">
+                                <FontAwesomeIcon icon={faEnvelope} size="lg" />
+                            </div>
+                            <input className="text-lg w-full" type="email" placeholder="Email" />
                         </div>
                         <div className="md:mr-20 h-12 border flex px-4 gap-4">
                             <div className="h-12 flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                                    <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3c0-2.9-2.35-5.25-5.25-5.25zm3.75 8.25v-3a3.75 3.75 0 10-7.5 0v3h7.5z" clip-rule="evenodd" />
-                                </svg>
+                                <FontAwesomeIcon icon={faLock} size="lg" />
                             </div>
-                            <input className="text-lg" type="password" placeholder="Password" />
+                            <input className="text-lg w-full" type="password" placeholder="Password" />
                         </div>
                         <div className="md:mt-5 flex gap-5">
-                            <button className="px-4 py-2 md:px-16 md:py-5 border bg-blue-400 text-white rounded-full font-bold">LOGIN</button>
-                            <a className="flex items-center text-xs md:text-sm text-gray-500 cursor-pointer">FORGET PASSWORD?</a>
+                            <button className="px-4 py-2 md:px-16 md:py-5 border bg-blue-400 hover:bg-blue-500 text-white rounded-full font-bold">{!reg ? "LOGIN" : "SIGN IN"}</button>
+                            {!reg && <a className="flex items-center text-xs md:text-sm text-gray-500 cursor-pointer">FORGET PASSWORD?</a>}
                         </div>
                         <div className="flex gap-12 md:mt-4">
-                            <p>Login with</p>
-                            <div className="flex md:gap-10 gap-5 ">
-                                <div><FontAwesomeIcon icon={faGoogle} size="xl" style={{color: "#2b3a55",}} className="cursor-pointer"/></div>
-                                <div><FontAwesomeIcon icon={faFacebook} size="xl" style={{color: "#2b3a55",}} className="cursor-pointer"/></div>
-                                <div><FontAwesomeIcon icon={faTwitter} size="xl" style={{color: "#2b3a55",}} className="cursor-pointer"/></div>
+                            <p className="md:p-2">{!reg ? "Login with" : "Sign In With"}</p>
+                            <div className="flex md:gap-3 gap-5 ">
+                                <div className="md:border md:p-2" onClick={()=>{signIn("google")}}><FontAwesomeIcon icon={faGoogle} size="xl" style={{ color: "#2b3a55", }} className="cursor-pointer" /></div>
+                                <div className="md:border md:p-2" onClick={()=>{signIn("facebook")}}><FontAwesomeIcon icon={faFacebook} size="xl" style={{ color: "#2b3a55", }} className="cursor-pointer" /></div>
+                                <div className="md:border md:p-2" onClick={()=>{signIn("twitter")}}><FontAwesomeIcon icon={faTwitter} size="xl" style={{ color: "#2b3a55", }} className="cursor-pointer" /></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
